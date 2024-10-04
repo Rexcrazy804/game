f535f785adf1cc8b6a30beb5f3b053d70658ac88
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-10-05 01:19:18 +0530

    RISE PHOE'NIX' basically remove the crappy makefile rewrite it in nix and finally EVERYTHING EVERYWHERE ALL AT ONCE :kirbynut:

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+
+    program = pkgs.stdenv.mkDerivation rec {
+      pname = "paddle-game";
+      version = "0.1.69";
+      src = ./.;
+
+      buildInputs = [
+        pkgs.sfml
+      ];
+
+      buildPhase = ''
+        g++ \
+          -c $src/src/*.cpp \
+          -I${pkgs.lib.makeIncludePath buildInputs}
+
+        g++ \
+          main.o -o paddle-game \
+          -L${pkgs.lib.makeLibraryPath buildInputs} \
+          -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio \
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp paddle-game $out/bin
+      '';
+    };
+  in {
+    packages.x86_64-linux.default = program;
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = [
+        pkgs.sfml
+      ];
+    };
+  };
+}
